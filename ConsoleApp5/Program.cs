85077b757cf0141c4d57209115c19018035cb3e0
Keli06001 <keli06001@utb.vaxjo.se>
2024-04-10 10:23:34 +0200

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public string FullName
+    {
+        get { return FirstName + " " + LastName; }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person();
+        DateTime Date1 = new DateTime(2005,12,22);
+
+        DateTime datetime2=DateTime.Now;
+        TimeSpan TimeSpan1 = TimeSpan.FromDays(18);
+ 
+        person.FirstName = "John";
+        person.LastName = "Doe";
+
+        Console.WriteLine("Full Name: " + person.FullName);
+
+        if (datetime2 - Date1 >= TimeSpan1)
+        {
+            Console.WriteLine("Du är över 18");
+        }
+        else
+        {
+            Console.WriteLine("Du är under 18");
+        }
+    }
+}
